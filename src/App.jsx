a1257db523aad931d2bafaf9ae92ecc851140c29
DaLiteralPanda <44968396+DaLiteralPanda@users.noreply.github.com>
2025-09-10 18:30:25 +0530

    Vite + React Setup has be completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Button, Stack, Title } from '@mantine/core';
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Stack align="center" mt={50}>
+      <Title>Welcome to JanSeva!</Title>
+      <Button>This is a Mantine Button</Button>
+    </Stack>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Mantine
+import { createTheme, MantineProvider } from '@mantine/core';
+
+// 1. Import Mantine's base styles -- VERY IMPORTANT!
+import '@mantine/core/styles.css';
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    {/* 2. Wrap your entire App component with the MantineProvider */}
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
+
+
+
+
