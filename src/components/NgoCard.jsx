a9d98579a0fc73b77b12b61c387fd5fdc9e22dad
DaLiteralPanda <44968396+DaLiteralPanda@users.noreply.github.com>
2025-09-11 17:41:31 +0530

    Made the corner radius better

--- src/components/NgoCard.jsx
+++ src/components/NgoCard.jsx
@@ -4,9 +4,9 @@ import classes from './NgoCard.module.css';
 // The new component structure
 export function NgoCard({ image, name, description, location, category}) {
   return (
-    <Card p="lg" radius="md" className={classes.card}>
+    <Card p="lg" radius="xl" className={classes.card}>
       <Card.Section className={classes.imageSection}>
-        <Image src={image} height={250} alt={name} radius="md" />
+        <Image src={image} height={250} alt={name} radius="lg" />
       </Card.Section>
 
       <Badge color="#6d381fff" variant="light" mt="xl" className={classes.badge}>
